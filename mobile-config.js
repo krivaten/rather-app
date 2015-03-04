9b84cffeff635844d3bf17998ae6f5dd079cd954
Kris Van Houten <kris.vanhouten@zipscene.com>
2015-03-04 14:33:16 -0500

    updated splash images and icons

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,8 @@
 App.info({
   name: 'Rather',
   description: 'Decisions made social',
-  author: 'Sexy Dude Apps',
+  author: '
+  ',
   email: 'krivaten@gmail.com',
   website: 'http://krivaten.com',
   version: '0.1.0'
@@ -9,37 +10,37 @@ App.info({
 
 App.icons({
   // iOS
-  'iphone': 'public/img/icons/icon_60x60.png',
-  'iphone_2x': 'public/img/icons/icon_120x120.png',
-  'ipad': 'public/img/icons/icon_72x72.png',
-  'ipad_2x': 'public/img/icons/icon_144x144.png',
+  'iphone': 'public/img/icons/ios-60x60.png',
+  'iphone_2x': 'public/img/icons/ios-120x120.png',
+  'ipad': 'public/img/icons/ios-72x72.png',
+  'ipad_2x': 'public/img/icons/ios-144x144.png',
 
   // Android
-  'android_ldpi': 'public/img/icons/icon_36x36.png',
-  'android_mdpi': 'public/img/icons/icon_48x48.png',
-  'android_hdpi': 'public/img/icons/icon_72x72.png',
-  'android_xhdpi': 'public/img/icons/icon_96x96.png'
+  'android_ldpi': 'public/img/icons/android-36x36.png',
+  'android_mdpi': 'public/img/icons/android-48x48.png',
+  'android_hdpi': 'public/img/icons/android-72x72.png',
+  'android_xhdpi': 'public/img/icons/android-96x96.png'
 });
 
 App.launchScreens({
   // iOS
-  'iphone': 'public/img/splash/splash_320x480.png',
-  'iphone_2x': 'public/img/splash/splash_640x960.png',
-  'iphone5': 'public/img/splash/splash_640x1136.png',
-  'ipad_portrait': 'public/img/splash/splash_768x1024.png',
-  'ipad_portrait_2x': 'public/img/splash/splash_1536x2048.png',
-  'ipad_landscape': 'public/img/splash/splash_1024x768.png',
-  'ipad_landscape_2x': 'public/img/splash/splash_2048x1536.png',
+  'iphone': 'public/img/splash/ios-320x480.jpg',
+  'iphone_2x': 'public/img/splash/ios-640x960.jpg',
+  'iphone5': 'public/img/splash/ios-640x1136.png',
+  'ipad_portrait': 'public/img/splash/ios-768x1024.jpg',
+  'ipad_portrait_2x': 'public/img/splash/ios-1536x2048.jpg',
+  'ipad_landscape': 'public/img/splash/ios-1024x768.jpg',
+  'ipad_landscape_2x': 'public/img/splash/ios-2048x1536.jpg',
 
   // Android
-  'android_ldpi_portrait': 'public/img/splash/splash_200x320.png',
-  'android_ldpi_landscape': 'public/img/splash/splash_320x200.png',
-  'android_mdpi_portrait': 'public/img/splash/splash_320x480.png',
-  'android_mdpi_landscape': 'public/img/splash/splash_480x320.png',
-  'android_hdpi_portrait': 'public/img/splash/splash_480x800.png',
-  'android_hdpi_landscape': 'public/img/splash/splash_800x480.png',
-  'android_xhdpi_portrait': 'public/img/splash/splash_720x1280.png',
-  'android_xhdpi_landscape': 'public/img/splash/splash_1280x720.png'
+  'android_ldpi_portrait': 'public/img/splash/android-200x320.jpg',
+  'android_ldpi_landscape': 'public/img/splash/android-320x200.jpg',
+  'android_mdpi_portrait': 'public/img/splash/android-320x480.jpg',
+  'android_mdpi_landscape': 'public/img/splash/android-480x320.jpg',
+  'android_hdpi_portrait': 'public/img/splash/android-480x800.jpg',
+  'android_hdpi_landscape': 'public/img/splash/android-800x480.jpg',
+  'android_xhdpi_portrait': 'public/img/splash/android-720x1280.jpg',
+  'android_xhdpi_landscape': 'public/img/splash/android-1280x720.jpg'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
