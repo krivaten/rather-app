0d0e78c5395d3083bf36f461c0adb921d680d8b0
Kris Van Houten <kris.vanhouten@zipscene.com>
2015-03-04 16:02:24 -0500

    update facebook and twiter services on startup

--- server/startup.js
+++ server/startup.js
@@ -2,13 +2,25 @@
  * Meteor startup
  */
 Meteor.startup(function() {
+
+	// Update Twitter configuration
 	Accounts.loginServiceConfiguration.remove({
-		service : 'twitter'
+		service: 'twitter'
+	});
+	Accounts.loginServiceConfiguration.insert({
+		service: 'twitter',
+		consumerKey: 'whsAlCJUIKT6niIeTzDoPv3Gr',
+		secret: 'XrnQ6spiZKiw8knbEfExMCKESVRzDiEPDqGVz15LWZ8VHxdp14'
 	});
 
+	// Update Facebook configuration
+	Accounts.loginServiceConfiguration.remove({
+		service: 'facebook'
+	});
 	Accounts.loginServiceConfiguration.insert({
-		service     : 'twitter',
-		consumerKey : 'whsAlCJUIKT6niIeTzDoPv3Gr',
-		secret      : 'XrnQ6spiZKiw8knbEfExMCKESVRzDiEPDqGVz15LWZ8VHxdp14'
+		service: 'facebook',
+		appId: 'whsAlCJUIKT6niIeTzDoPv3Gr',
+		secret: 'XrnQ6spiZKiw8knbEfExMCKESVRzDiEPDqGVz15LWZ8VHxdp14',
+		loginStyle: 'popup'
 	});
 });
