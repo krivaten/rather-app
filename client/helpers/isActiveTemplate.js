06d5ceeddeba132d02e76a97ebb4bfc7862fab3d
Kris Van Houten <krivaten@gmail.com>
2015-03-08 03:27:17 -0400

    added a global isActiveTemplate helper

---
+++ client/helpers/isActiveTemplate.js
@@ -0,0 +1,22 @@
+/**
+ * Determine if active template is the passed value
+ *
+ * @method isActiveTemplate
+ * @param {String} string Value to determine if active template
+ * @return {Boolean} Whether or not the current route matches the passed route
+ * @since v0.0.1
+ */
+var isActiveTemplate = function(template) {
+	var currentRoute = Router.current();
+
+	template = template.toLowerCase();
+
+	return currentRoute && template === currentRoute.lookupTemplate().toLowerCase();
+}
+
+Template.registerHelper('isActiveTemplate', isActiveTemplate);
+
+/**
+ * Example:
+ * {{isActiveTemplate "info/terms-of-service}}
+ */
