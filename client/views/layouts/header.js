65d20c2e85ac3252b19f4576c472d22b27b94ac8
Kris Van Houten <krivaten@gmail.com>
2015-03-08 03:27:46 -0400

    smarter layout controls

--- client/views/layouts/header.js
+++ client/views/layouts/header.js
@@ -27,21 +27,5 @@ Template.layoutHeader.helpers({
 	 */
 	hideCompleted: function () {
 		return Session.get("hideCompleted");
-	},
-
-
-	/**
-	 * Determine if active template is the passed value
-	 *
-	 * @method isActiveTemplate
-	 * @return {Boolean} Whether or not the current route matches the passed route
-	 * @since v0.1.0
-	 */
-	isActiveTemplate: function (template) {
-		var currentRoute = Router.current();
-
-		template = template.toLowerCase();
-
-		return currentRoute && template === currentRoute.lookupTemplate().toLowerCase();
 	}
 });
