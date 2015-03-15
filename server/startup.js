b408b73af10b091285b0b6e8af47e423c88aef14
Kris Van Houten <krivaten@gmail.com>
2015-03-15 15:41:01 -0400

    enabled the use of different api keys depending on the environment

--- server/startup.js
+++ server/startup.js
@@ -2,6 +2,29 @@
  * Meteor startup
  */
 Meteor.startup(function() {
+	var serverMode, mailURL, twitterConsumerKey, twitterSecret, facebookId, facebookSecret;
+
+	switch (Meteor.absoluteUrl()) {
+		case ('http://ratherapp.com'):
+			serverMode = 'production';
+			break;
+
+		case ('http://rather.meteor.com'):
+			serverMode = 'test';
+			break;
+
+		default:
+			serverMode = 'development';
+			mailURL = 'smtp://postmaster%40sandbox373c80d5cce344dd88dfc412b97dfc78.mailgun.org:3789f6f3598031d264790789f3fb8c61@smtp.mailgun.org:587';
+			twitterConsumerKey = 'whsAlCJUIKT6niIeTzDoPv3Gr';
+			twitterSecret = 'XrnQ6spiZKiw8knbEfExMCKESVRzDiEPDqGVz15LWZ8VHxdp14';
+			facebookId = '1558392507753480';
+			facebookSecret = 'b5b26d0904667b7c98568f9af857656a';
+			break;
+	}
+
+	// Establish Mail url
+	process.env.MAIL_URL = mailURL;
 
 	// Update Twitter configuration
 	Accounts.loginServiceConfiguration.remove({
@@ -10,8 +33,8 @@ Meteor.startup(function() {
 
 	Accounts.loginServiceConfiguration.insert({
 		service: 'twitter',
-		consumerKey: 'whsAlCJUIKT6niIeTzDoPv3Gr',
-		secret: 'XrnQ6spiZKiw8knbEfExMCKESVRzDiEPDqGVz15LWZ8VHxdp14'
+		consumerKey: twitterConsumerKey,
+		secret: twitterSecret
 	});
 
 	// Update Facebook configuration
@@ -21,8 +44,8 @@ Meteor.startup(function() {
 
 	Accounts.loginServiceConfiguration.insert({
 		service: 'facebook',
-		appId: '1558392507753480',
-		secret: 'b5b26d0904667b7c98568f9af857656a',
+		appId: facebookId,
+		secret: facebookSecret,
 		loginStyle: 'popup'
 	});
 });
