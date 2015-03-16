c2ca88c318dda5b8fa4910a375b2f0d316ba8fc3
Kris Van Houten <krivaten@gmail.com>
2015-03-16 12:52:42 -0400

    added comments and moved a couple things around

--- server/startup.js
+++ server/startup.js
@@ -5,14 +5,23 @@ Meteor.startup(function() {
 	var serverMode, mailURL, twitterConsumerKey, twitterSecret, facebookId, facebookSecret;
 
 	switch (Meteor.absoluteUrl()) {
+
+		// Production
 		case ('http://ratherapp.com'):
 			serverMode = 'production';
 			break;
 
+		// Test
 		case ('http://rather.meteor.com'):
 			serverMode = 'test';
+			mailURL = 'smtp://postmaster%40sandbox373c80d5cce344dd88dfc412b97dfc78.mailgun.org:3789f6f3598031d264790789f3fb8c61@smtp.mailgun.org:587';
+			twitterConsumerKey = 'whsAlCJUIKT6niIeTzDoPv3Gr';
+			twitterSecret = 'XrnQ6spiZKiw8knbEfExMCKESVRzDiEPDqGVz15LWZ8VHxdp14';
+			facebookId = '1558392507753480';
+			facebookSecret = 'b5b26d0904667b7c98568f9af857656a';
 			break;
 
+		// Development
 		default:
 			serverMode = 'development';
 			mailURL = 'smtp://postmaster%40sandbox373c80d5cce344dd88dfc412b97dfc78.mailgun.org:3789f6f3598031d264790789f3fb8c61@smtp.mailgun.org:587';
