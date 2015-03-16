dc94284ab8435f336cb919921255a432eb7e678d
Kris Van Houten <krivaten@gmail.com>
2015-03-16 12:53:33 -0400

    added stub for emails and templates

---
+++ server/email.js
@@ -0,0 +1,35 @@
+/**
+ * Email configuration
+ */
+Accounts.emailTemplates.siteName = "Rather";
+Accounts.emailTemplates.from = "Rather <accounts@bewm.com>";
+
+/**
+Accounts.emailTemplates.resetPassword.subject.subject = function (user) {
+    return "Welcome to Awesome Town, " + user.profile.name;
+};
+Accounts.emailTemplates.resetPassword.subject.text = function (user, url) {
+   return "You have been selected to participate in building a better future!"
+     + " To activate your account, simply click the link below:\n\n"
+     + url;
+};
+Accounts.emailTemplates.resetPassword.subject.html = function (user, url) {
+   return "You have been selected to participate in building a better future!"
+     + " To activate your account, simply click the link below:\n\n"
+     + url;
+};
+
+Accounts.emailTemplates.enrollAccount.subject = function (user) {
+    return "Welcome to Awesome Town, " + user.profile.name;
+};
+Accounts.emailTemplates.enrollAccount.text = function (user, url) {
+   return "You have been selected to participate in building a better future!"
+     + " To activate your account, simply click the link below:\n\n"
+     + url;
+};
+Accounts.emailTemplates.enrollAccount.html = function (user, url) {
+   return "You have been selected to participate in building a better future!"
+     + " To activate your account, simply click the link below:\n\n"
+     + url;
+};
+*/
