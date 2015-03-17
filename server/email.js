dba7aeed2e8e127bc6e36c2e85b202e84bc59c33
Kris Van Houten <krivaten@gmail.com>
2015-03-16 23:28:18 -0400

    added custom text for reset password email

--- server/email.js
+++ server/email.js
@@ -4,32 +4,21 @@
 Accounts.emailTemplates.siteName = "Rather";
 Accounts.emailTemplates.from = "Rather <accounts@bewm.com>";
 
-/**
-Accounts.emailTemplates.resetPassword.subject.subject = function (user) {
-    return "Welcome to Awesome Town, " + user.profile.name;
-};
-Accounts.emailTemplates.resetPassword.subject.text = function (user, url) {
-   return "You have been selected to participate in building a better future!"
-     + " To activate your account, simply click the link below:\n\n"
-     + url;
-};
-Accounts.emailTemplates.resetPassword.subject.html = function (user, url) {
-   return "You have been selected to participate in building a better future!"
-     + " To activate your account, simply click the link below:\n\n"
-     + url;
-};
 
-Accounts.emailTemplates.enrollAccount.subject = function (user) {
-    return "Welcome to Awesome Town, " + user.profile.name;
+Accounts.emailTemplates.resetPassword.subject = function (user) {
+    return "Reset your Rather account password";
 };
-Accounts.emailTemplates.enrollAccount.text = function (user, url) {
-   return "You have been selected to participate in building a better future!"
-     + " To activate your account, simply click the link below:\n\n"
+Accounts.emailTemplates.resetPassword.text = function (user, url) {
+   return "So it looks like you forgot your password. Bummer. I guess we can't all be like Johannes Mallow.\n\n"
+     + "Anyway, here is a link to reset your password. Cheers!\n\n"
      + url;
 };
-Accounts.emailTemplates.enrollAccount.html = function (user, url) {
-   return "You have been selected to participate in building a better future!"
-     + " To activate your account, simply click the link below:\n\n"
-     + url;
+Accounts.emailTemplates.resetPassword.html = function (user, url) {
+   return "So it looks like you forgot your password.<br /><br />"
+     + "Bummer.<br /><br />"
+     + "I guess we can't all be like Johannes Mallow.<br /><br />"
+     + "Anyway, here is a link to reset your password.<br /><br />"
+     + "<a href=\"" + url + "\" target=\"_blank\">" + url + "</a><br /><br />"
+     + "Cheers!<br />"
+     + "<strong>Team Rather</strong>";
 };
-*/
