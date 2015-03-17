96a31d45011b364f80bc55b5672360e022874026
Kris Van Houten <krivaten@gmail.com>
2015-03-16 23:27:11 -0400

    added basic support for custom lodash mixins

--- public/js/helpers/lodash-helpers.js
+++ public/js/helpers/lodash-helpers.js
@@ -1,3 +1,18 @@
 /**
  * Lodash helpers
  */
+var lodashHelpers = {};
+
+/**
+ * Check to make sure a value is present
+ *
+ * @method isPresent
+ * @param {Any} value Value to check for presence
+ * @return {Boolean} [description]
+ * @since v0.1.0
+ */
+lodashHelpers.isPresent = function(value) {
+	return (!_.isUndefined(value) && !_.isNull(value));
+};
+
+_.mixin(lodashHelpers);
