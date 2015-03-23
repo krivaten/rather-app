e17f4b5505fd8a47ad515692e7a8aa0840a4ec5f
Kris Van Houten <krivaten@gmail.com>
2015-03-23 10:54:59 -0400

    created change-password flow

---
+++ client/views/routes/account/change-password.js
@@ -0,0 +1,42 @@
+/**
+ * Account change password
+ */
+Template.accountChangePassword.events({
+
+
+	/**
+	 * Update password
+	 *
+	 * @since v0.1.0
+	 */
+	'submit form': function(event, template) {
+		var user, errors, target, currentPassword, newPassword;
+
+		// Prevent default form behavior
+		event.preventDefault();
+
+		// Shortcuts
+		user = Meteor.user();
+		errors = [];
+		target = event.target;
+		currentPassword = target.currentPassword.value;
+		newPassword = target.newPassword.value;
+
+		// Make sure required fields are set
+		if (_.isEmpty(currentPassword)) errors.push(messages.fields.currentPassword);
+		if (_.isEmpty(newPassword)) errors.push(messages.fields.newPassword);
+
+		if (_.isEmpty(errors) && currentPassword === newPassword) errors.push(messages.fields.identicalPassword);
+
+		// If any errors, display them
+		if (!_.isEmpty(errors)) return messages.errors.list(errors);
+
+		Accounts.changePassword(currentPassword, newPassword, function(error) {
+			if (error) return messages.errors.general(error);
+
+			messages.account.updatedPassword();
+
+			Router.go('accountIndex');
+		});
+	}
+});
