bfe250885ee49ade7665b2f7ed838f7406f0a17b
Kris Van Houten <krivaten@gmail.com>
2015-03-23 10:54:40 -0400

    cleaned up function spaces

--- server/email.js
+++ server/email.js
@@ -5,15 +5,15 @@ Accounts.emailTemplates.siteName = "Rather";
 Accounts.emailTemplates.from = "Rather <accounts@bewm.com>";
 
 
-Accounts.emailTemplates.resetPassword.subject = function (user) {
+Accounts.emailTemplates.resetPassword.subject = function(user) {
     return "Reset your Rather account password";
 };
-Accounts.emailTemplates.resetPassword.text = function (user, url) {
+Accounts.emailTemplates.resetPassword.text = function(user, url) {
    return "So it looks like you forgot your password. Bummer. I guess we can't all be like Johannes Mallow.\n\n"
      + "Anyway, here is a link to reset your password. Cheers!\n\n"
      + url;
 };
-Accounts.emailTemplates.resetPassword.html = function (user, url) {
+Accounts.emailTemplates.resetPassword.html = function(user, url) {
    return "So it looks like you forgot your password.<br /><br />"
      + "Bummer.<br /><br />"
      + "I guess we can't all be like Johannes Mallow.<br /><br />"
